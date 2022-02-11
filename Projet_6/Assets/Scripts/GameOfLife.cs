808e4581d5a78fbc6e1faee820a756b876708381
rafagonz501 <66130229+rafagonz501@users.noreply.github.com>
2022-02-11 17:50:04 +0100

    scripts

--- Projet_6/Assets/Scripts/GameOfLife.cs
+++ Projet_6/Assets/Scripts/GameOfLife.cs
@@ -1,33 +1,139 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+ 
 
 public class GameOfLife : MonoBehaviour
 {
-    [Range(0, 20)]
-    public float speed = 5f;
+    public float _stepDelay;
+    [SerializeField] private GridManager _gridManager;
+    [SerializeField] private Material _aliveMaterial;
+    [SerializeField] private Material _deadMaterial;
 
-    private float timer = 0;
-
-
-    void Update()
+    private void Awake()
     {
-        //CountNeighbour();
+     
+        Object obj = FindObjectOfType(typeof(GridManager));
+        _gridManager = (GridManager)obj;
+        _gridManager = obj as GridManager;
+        _gridManager = FindObjectOfType<GridManager>();
+        FindObjectOfType<GridManager>();
 
-
-
+        
     }
 
+    //private void SimulationStep()
+    //{
+    //    Debug.Log("SimulationStep");
 
-    public void simulationSpeed(float newSpeed)
+    //    int n = CountNeighbors(col, row);
+    //}
+
+    public void CountNeighbors()
     {
-        speed = newSpeed;
-        if (timer < speed)
+        
+
+        for (int i = 0; i < _gridManager.m_NumRows; i++)
         {
-            timer = 0f;
+            for (int j = 0; j < _gridManager.m_NumCol; j++)
+            {
+                var cell = GridManager.Instance.m_Grid[j, i];
+                var meshRenderer = cell.GetComponentInChildren<MeshRenderer>();
+
+                cell.m_Neighbors = 0;
+
+                if (i - 1 >= 0 && i - 1 < _gridManager.m_NumRows)//down
+                {
+                    var otherCell = GridManager.Instance.m_Grid[j, i - 1];
+                    if (otherCell.m_isAlive == true)
+                    {
+                        cell.m_Neighbors += 1;
+                    }
+                }
+                if (i + 1 >= 0 && i + 1 < _gridManager.m_NumRows)//up
+                {
+                    var otherCell = GridManager.Instance.m_Grid[j, i + 1];
+                    if (otherCell.m_isAlive == true)
+                    {
+                        cell.m_Neighbors += 1;
+                    }
+                }
+                if (j - 1 >= 0 && j - 1 < _gridManager.m_NumRows)//left
+                {
+                    var otherCell = GridManager.Instance.m_Grid[j - 1, i];
+                    if (otherCell.m_isAlive == true)
+                    {
+                        cell.m_Neighbors += 1;
+                    }
+                }
+                if (j + 1 >= 0 && j + 1 < _gridManager.m_NumRows)//right
+                {
+                    var otherCell = GridManager.Instance.m_Grid[j + 1, i];
+                    if (otherCell.m_isAlive == true)
+                    {
+                        cell.m_Neighbors += 1;
+                    }
+                }
+                if (i + 1 >= 0 && i + 1 < _gridManager.m_NumRows &&
+                    j - 1 >= 0 && j - 1 < _gridManager.m_NumRows)//diagonal up left
+                {
+                    var otherCell = GridManager.Instance.m_Grid[j - 1, i + 1];
+                    if (otherCell.m_isAlive == true)
+                    {
+                        cell.m_Neighbors += 1;
+                    }
+                }
+                if (i + 1 >= 0 && i + 1 < _gridManager.m_NumRows &&
+                    j + 1 >= 0 && j + 1 < _gridManager.m_NumRows)//diagonal up right
+                {
+                    var otherCell = GridManager.Instance.m_Grid[j + 1, i + 1];
+                    if (otherCell.m_isAlive == true)
+                    {
+                        cell.m_Neighbors += 1;
+                    }
+                }
+                if (i - 1 >= 0 && i - 1 < _gridManager.m_NumRows &&
+                    j - 1 >= 0 && j - 1 < _gridManager.m_NumRows)//diagonal down left
+                {
+                    var otherCell = GridManager.Instance.m_Grid[j - 1, i - 1];
+                    if (otherCell.m_isAlive == true)
+                    {
+                        cell.m_Neighbors += 1;                      
+                    }
+                }
+                if (i - 1 >= 0 && i - 1 < _gridManager.m_NumRows &&
+                    j + 1 >= 0 && j + 1 < _gridManager.m_NumRows)//diagonal down right
+                {
+                    var otherCell = GridManager.Instance.m_Grid[j + 1, i - 1];
+                    if (otherCell.m_isAlive == true)
+                    {
+                        cell.m_Neighbors += 1;                        
+                    }
+                }
+            }
         }
-        else{
-            timer += Time.deltaTime;
+        for (int i = 0; i < _gridManager.m_NumRows; i++)
+        {
+            for (int j = 0; j < _gridManager.m_NumCol; j++)
+            {
+                var cell = GridManager.Instance.m_Grid[j, i];
+                var meshRenderer = cell.GetComponentInChildren<MeshRenderer>();
+
+                
+                if (cell.m_Neighbors == 3)// 3 neighbors -> alive
+                {
+                    cell.m_isAlive = true;
+                    meshRenderer.sharedMaterial = _aliveMaterial;
+                }
+                if (cell.m_Neighbors <= 1 || cell.m_Neighbors > 3)// 1 or more than 3 neighbors -> dead
+                {
+                    cell.m_isAlive = false;
+                    meshRenderer.sharedMaterial = _deadMaterial;
+                }
+
+            }
         }
-    }
+        
+    }                
+        
 }
